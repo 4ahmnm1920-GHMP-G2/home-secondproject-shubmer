5e90d44a2ebb32344de696a929c72a8cdc656794
Stefanie Hubmer <stefanie.hubmer@htl-salzburg.ac.at>
2019-12-03 14:55:52 +0100

    Added Script

---
+++ Assets/MyGame/Scripts/HelloWorldFromHome.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorldFromHome : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello From HOME, \n Greetings shubmer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
